78599efa235594c8e16de40adf5ad9af6bfe5b15
jrgondim <79653003+jrgondim@users.noreply.github.com>
2022-10-11 08:34:26 -0300

    Add files via upload

--- curso-javascript-poo/index.js
+++ curso-javascript-poo/index.js
@@ -11,6 +11,7 @@ const cliente6 = new Cliente("Deodora Fran", 886643388888);
 const conta1 = new ContaCorrente(1001, cliente1);
 conta1.depositar(500);
 conta1.sacar(100);
+conta1.tranferir(200, conta2);
 
 const conta2 = new ContaCorrente(102, cliente2);
 conta2.depositar(10000);
@@ -26,14 +27,6 @@ conta5.depositar(700);
 
 const conta6 = new ContaCorrente(102, cliente6);
 conta6.depositar(100);
-
-
-
-
-
-
-
-conta1.tranferir(200, conta2);
 
 console.log(conta1);
 console.log(conta2);
